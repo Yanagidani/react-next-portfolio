5de29c866ad6c2febfa6e3042a2c302b8f726cef
Yanagidani <ktc24a31e0018@edu.kyoto-tech.ac.jp>
2025-02-01 16:12:07 +0900

    yuki

--- app/_components/Hero/index.tsx
+++ app/_components/Hero/index.tsx
@@ -6,6 +6,21 @@ type Props ={
     sub: string;
 };
 
+function generateStars() {
+    const stars = [];
+    for (let i = 0; i < 100; i++) {
+      const style = {
+        top: `-${3}vh`, // ランダムなY位置
+        left: `${Math.random() * 100}vw`, // ランダムなX位置
+        animationDelay: `${Math.random() * 5}s`, // ランダムな開始タイミング
+        animationDuration: `${4 + Math.random() * 3}s`, // ランダムな速度
+      };
+      stars.push(<div key={i} className={styles.star} style={style}></div>);
+    }
+    return stars;
+  }
+
+
 export default function Hero({ title, sub }: Props) {
     return (
         <section className={styles.container}>
@@ -13,6 +28,8 @@ export default function Hero({ title, sub }: Props) {
                 <h1 className={styles.title}>{title}</h1>
                 <p className={styles.sub}>{sub}</p>
             </div>
+            {/* 星を生成 */}
+                {generateStars()}       
             <Image
                 className={styles.bgimg}
                 src="/a.jpg"
